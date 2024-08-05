42e6bf412ffd97652ed80f32e1e037b8f4677f3d
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-08-05 09:06:00 +0200

    Change all fonts to sans-serif

--- Changes.pm
+++ Changes.pm
@@ -158,6 +158,7 @@ sub _process_css {
 		['d', 'padding', '20px'],
 		['d', 'border-radius', '8px'],
 		['d', 'box-shadow', '0 2px 4px rgba(0, 0, 0, 0.1)'],
+		['d', 'font-family', 'Arial, Helvetica, sans-serif'],
 		['e'],
 
 		['s', '.'.$self->{'css_class'}.' .version'],
